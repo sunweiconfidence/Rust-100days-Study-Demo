5cf9aa5439db1c1d598dc13f682538da27e30a2e
sunwei <418531356@qq.com>
2022-04-06 21:34:53 +0800

    update

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,5 +7,15 @@ fn main() {
     //move example
     let s1 = String::from("Hello");
     let s2 = s1;
-    println!("{},{}",s1,s2) //s1 can't use, because has been freed
+    println!("{},{}",s1,s2); //s1 can't use, because has been freed
+
+    //copy example, stored in stack
+    let x = 5;
+    let y = x;
+    println!("{},{}", x, y);
+
+    //clone example, 
+    let s3: String = String::from("hello");
+    let s4 = s3.clone();
+    println!("{},{}", s3, s4);
 }
